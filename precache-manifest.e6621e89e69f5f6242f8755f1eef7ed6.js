d1a2862383c5aa3c42ce9a5e3f8013ec222e7d7f
Mike Shohet <mshohet@Mikes-MacBook-Pro.local>
2020-08-11 18:15:28 -0400

    Updates

---
+++ precache-manifest.e6621e89e69f5f6242f8755f1eef7ed6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75e14fd11875cd7fcf3fe27254e856da",
+    "url": "/robobook/index.html"
+  },
+  {
+    "revision": "fe3e7ab796bcb450f64e",
+    "url": "/robobook/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "1fc718c2438efd0b9b18",
+    "url": "/robobook/static/css/main.0b8baf75.chunk.css"
+  },
+  {
+    "revision": "fe3e7ab796bcb450f64e",
+    "url": "/robobook/static/js/2.f7cb7955.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robobook/static/js/2.f7cb7955.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fc718c2438efd0b9b18",
+    "url": "/robobook/static/js/main.68c07a99.chunk.js"
+  },
+  {
+    "revision": "1c551495d77710543d19",
+    "url": "/robobook/static/js/runtime-main.6cdd9c70.js"
+  },
+  {
+    "revision": "282fa349d2a8470e55be011fb05e519c",
+    "url": "/robobook/static/media/joystix.282fa349.ttf"
+  }
+]);
