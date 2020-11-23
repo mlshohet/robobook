21442930ec968a0a4df877b71b0648b08de477fa
Mike Shohet <mlshohet@gmail.com>
2020-11-23 17:59:53 -0500

    added icons for pwa compatibility, updated README

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,6 +4,7 @@ const SearchBox = ({searchBox, searchChange}) => {
 	return (
 		<div>
 			<input
+				aria-label='Search Robots'
 				className = 'pa3 mb2'
 				type='search'
 				placeholder='search robots'
